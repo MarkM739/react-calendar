35b5e4a2ab0a8c4c9383e294aa590ee4079925f7
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2023-03-04 16:21:26 -0330

    Callback prop

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -19,6 +19,36 @@ export default function Dropdown(props: DropdownProps) {
         })
     }
 
+    class GetInput extends React.Component {
+      constructor(props) {
+        super(props);
+      }
+      render() {
+        return (
+          <div>
+            <h3>Get Input:</h3>
+            <input
+              value={this.props.input}
+              onChange={this.props.handleChange}/>
+          </div>
+        );
+      }
+    };
+    
+    class RenderInput extends React.Component {
+      constructor(props) {
+        super(props);
+      }
+      render() {
+        return (
+          <div>
+            <h3>Input Render:</h3>
+            <p>{this.props.input}</p>
+          </div>
+        );
+      }
+    };
+
   return (
     <>
       <label
