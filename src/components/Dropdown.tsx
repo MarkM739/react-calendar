3a0e9b0c38cbfd810b3b40db28d5910d82c1f24f
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2023-03-06 19:57:42 -0330

    Update Dropdown.tsx

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -49,6 +49,35 @@ export default function Dropdown(props: DropdownProps) {
       }
     };
 
+    
+class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      justClicked: null,
+      letters: Array.from({length: 26}, (_, i) => String.fromCharCode(A + i))
+    };
+  }
+  handleClick(letter) {
+    this.setState({ justClicked: letter });
+  }
+  render() {
+    return (
+      <div>
+        Just clicked: {this.state.justClicked}
+        <ul>
+          {this.state.letters.map(letter =>
+            <li key={letter} onClick={() => this.handleClick(letter)}>
+              {letter}
+            </li>
+          )}
+        </ul>
+      </div>
+    )
+  }
+}
+
+
   return (
     <>
       <label
