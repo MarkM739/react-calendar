20d4202e72ce11b3934210f83df597abd7f812c6
Mark Murphy <108417914+MarkM739@users.noreply.github.com>
2023-05-04 21:55:29 -0230

    Update Workouts.tsx

--- src/components/Workouts.tsx
+++ src/components/Workouts.tsx
@@ -54,3 +54,21 @@ interface WorkoutProps {
   };
   
   export default Workouts;
+
+
+//   New Fitness Tracker/Calendar??
+
+// 1. User registration and login: The user would have to register with their email address or social media account to access the app. The user would have a profile page where they can set their fitness goals, input their physical information, and track their progress.
+//  Something new I would have to learn. Creating accounts, storing data AND using multiple pages
+
+// 2. Exercise library: The app would have a library of exercises categorized by muscle groups or fitness goals. The user would be able to choose exercises from the library to add to their fitness program. 
+// Easy to do - can maybe look at something to generate different routines based on goals.
+
+// 3. Fitness program creation: The user would be able to create a fitness program by selecting exercises from the exercise library and adding them to their calendar. The user would be able to set the frequency and duration of their workout sessions, as well as the intensity level. 
+//Custom fitness plan makers that pulls items //
+
+// 4. Calendar: The calendar would be the main feature of the app, displaying the user's fitness program, including the exercises to be performed each day and the duration of each session. The user would be able to view their calendar by day, week, or month. 
+// Use code from Luxon on current calendar.//
+
+// 5. Workout tracking: The user would be able to track their progress by logging their workout sessions. The app would record the exercises performed, the duration of the workout, and the number of sets and repetitions completed. The app may also provide suggestions for modifying the workout program based on the user's progress.
+// The app can take data from either fitbit OR applewatch and use it (I own niether of the sort so wouldn't know too much about this..//
